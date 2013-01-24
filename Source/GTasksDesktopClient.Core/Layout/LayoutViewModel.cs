03d3e558cdb09d82b4e9cc987fd0dee8b23191ff
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-01-25 00:54:36 +0100

    TasksListsViewModel in layout

--- Source/GTasksDesktopClient.Core/Layout/LayoutViewModel.cs
+++ Source/GTasksDesktopClient.Core/Layout/LayoutViewModel.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
 using GTasksDesktopClient.Core.Lists;
 using Google.Apis.Tasks.v1.Data;
-using System.Linq;
 
 namespace GTasksDesktopClient.Core.Layout
 {
     public class LayoutViewModel
     {
-        public ObservableCollection<TasksListViewModel> TasksLists { get; private set; }
+        public TasksListsViewModel TasksListsViewModel { get; set; }
 
-        public LayoutViewModel(IEnumerable<TaskList> tasksLists)
+        public LayoutViewModel(
+            IEnumerable<TaskList> tasksLists, 
+            Func<IEnumerable<TaskList>, TasksListsViewModel> tasksListsViewModelFactory)
         {
-            var tasksListsViewModel = tasksLists.Select(tasksList => new TasksListViewModel(tasksList));
-            TasksLists = new ObservableCollection<TasksListViewModel>(tasksListsViewModel);
+            TasksListsViewModel = tasksListsViewModelFactory(tasksLists);
         }
     }
 }
---
+++ Source/GTasksDesktopClient.Core/Lists/TasksListsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Google.Apis.Tasks.v1.Data;
+
+namespace GTasksDesktopClient.Core.Lists
+{
+    public class TasksListsViewModel
+    {
+        public ObservableCollection<TasksListViewModel> TasksLists { get; private set; }
+
+        public TasksListsViewModel(IEnumerable<TaskList> tasksLists)
+        {
+            var tasksListsViewModel = tasksLists.Select(tasksList => new TasksListViewModel(tasksList));
+            TasksLists = new ObservableCollection<TasksListViewModel>(tasksListsViewModel);
+        }
+    }
+}
