b8fd118656cc5858276e8f7d358183375341ddb6
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-01-30 18:42:13 +0100

    Interface fixes

--- Source/GTasksDesktopClient.Core/Layout/LayoutViewModel.cs
+++ Source/GTasksDesktopClient.Core/Layout/LayoutViewModel.cs
@@ -1,10 +1,11 @@
-﻿using GTasksDesktopClient.Core.Synchronization;
+﻿using Caliburn.Micro;
+using GTasksDesktopClient.Core.Synchronization;
 using GTasksDesktopClient.Core.Tasks;
 using GTasksDesktopClient.Core.TasksLists;
 
 namespace GTasksDesktopClient.Core.Layout
 {
-    public class LayoutViewModel
+    public class LayoutViewModel : Conductor<IScreen>.Collection.OneActive
     {
         public TasksListsViewModel TasksListsViewModel { get; set; }
         public TasksViewModel TasksViewModel { get; set; }
