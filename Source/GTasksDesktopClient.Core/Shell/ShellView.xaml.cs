0fac97910e42a3cc4ceb21a72fdf9538460a1ddc
Jakub Sikorski <jakubsikorski7@gmail.com>
2013-01-30 17:50:21 +0100

    Metro style, Whistler Blue removed

--- Source/GTasksDesktopClient.Core/Shell/ShellView.xaml.cs
+++ Source/GTasksDesktopClient.Core/Shell/ShellView.xaml.cs
@@ -1,11 +1,12 @@
 using System.Windows.Controls;
+using MahApps.Metro.Controls;
 
 namespace GTasksDesktopClient.Core.Shell
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : UserControl
+    public partial class ShellView : MetroWindow
     {
         public ShellView()
         {
